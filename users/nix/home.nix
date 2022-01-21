2fe811ba5d86642aa6b9e8165da563f7d2343c23
Nebojsa Koturovic <contact@kotur.me>
2022-01-21 15:18:43 +0100

    Initial commit

---
+++ users/nix/home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "nix";
+  home.homeDirectory = "/home/nix";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+
+  home.packages = with pkgs; [
+      alacritty
+      neovim
+      git
+      git-crypt
+      gnupg
+      neofetch
+      pinentry_qt
+  ];
+
+  home.file = {
+    ".config/alacritty/alacritty.yaml".text = ''
+      env:
+        TERM: xterm-256color
+    '';
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.gpg = {
+      enable = true;
+  };
+
+  services.gpg-agent = {
+      enable = true;
+      pinentryFlavor = "qt";
+  };
+}
