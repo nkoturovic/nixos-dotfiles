235488769534d1c166df6fb88f38f8da61ba0472
Nebojsa Koturovic <contact@kotur.me>
2023-12-05 23:25:42 +0100

    Fix home-manager activation package

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#kotur@kotur-pc'
     homeConfigurations = {
-      "kotur@kotur-pc" = home-manager.lib.homeManagerConfiguration {
+      "kotur" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
