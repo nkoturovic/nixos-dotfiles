fc7ea408522934d063cebefe7da637bdb643b4d5
Nebojsa Koturovic <contact@kotur.me>
2024-05-31 11:10:25 +0200

    Make zsh default for nix-shell

--- home-manager/kotur.home.nix
+++ home-manager/kotur.home.nix
@@ -215,6 +215,19 @@
       source ${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search/zsh-fzf-history-search.plugin.zsh
     '';
 
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
+
     oh-my-zsh = {
       enable = true;
       plugins = [
